db0a2da89c682407b1b86e1d64953cebf7719304
Achintha Gunasekara <achintha@me.com>
2015-10-20 08:58:46 +1100

    Update ConfigFileReader.java

--- src/Config/ConfigFileReader.java
+++ src/Config/ConfigFileReader.java
@@ -1,15 +1,17 @@
+/*
+ * This application is deisnged to Synchronize files between a given
+ * directory and AWS S3 Bucket
+ *
+ * @author Archie Gunasekara
+ * @date 2015.05.12
+ * 
+ */
+ 
 package Config;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
-/*
- *
- * @author Archie Gunasekara
- * @date 2015.06.02
- * 
- */
 
 public class ConfigFileReader {
 	
