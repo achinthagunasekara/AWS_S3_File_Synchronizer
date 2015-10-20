bd6e3060dc117a4adcdb883fa5a3577edbe2f793
Achintha Gunasekara <achintha@me.com>
2015-10-21 08:59:58 +1100

    Update SyncMethod.java

--- src/Config/SyncMethod.java
+++ src/Config/SyncMethod.java
@@ -1,11 +1,14 @@
-package Config;
-
 /*
+ * This application is deisnged to Synchronize files between a given
+ * directory and AWS S3 Bucket
  *
  * @author Archie Gunasekara
- * @date 2015.06.02
+ * @date 2015.05.12
  * 
  */
+ 
+package Config;
+
 public class SyncMethod {
     
     public enum Methods {
